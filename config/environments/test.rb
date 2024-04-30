570486bf3c2848ae940209d7455662fb4c95f16d
Gerry Steven <sgerrys93@gmail.com>
2024-04-30 14:08:36 +0700

    Fix: Hosts

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -61,4 +61,11 @@ Rails.application.configure do
 
   # Raise error when a before_action's only/except options reference missing actions
   config.action_controller.raise_on_missing_callback_actions = true
+
+  config.hosts = [
+    IPAddr.new("0.0.0.0/0"),        # All IPv4 addresses.
+    IPAddr.new("::/0"),     
+    "localhost",                    # The localhost reserved domain.
+    ENV["RAILS_ALLOWED_HOSTS"]
+  ]
 end
